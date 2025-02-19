f965ce8bbaa3ab5bedde6c508a379e52c899b95a
CPenalba <carolinapenalbacorpa@gmail.com>
2025-02-19 13:20:25 +0100

    Add project files.

---
+++ McvNetCoreUtilidades/Controllers/CachingController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace McvNetCoreUtilidades.Controllers
+{
+    public class CachingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult MemoriaDistribuida()
+        {
+            string fecha = DateTime.Now.ToLongDateString() + " -- " + DateTime.Now.ToLongTimeString();
+            ViewData["FECHA"] = fecha;
+            return View();
+        }
+    }
+}
---
+++ McvNetCoreUtilidades/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace McvNetCoreUtilidades.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
